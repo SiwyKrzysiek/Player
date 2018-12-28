61d91ad9a59cac76117911e989ce7273df7432a1
Krzysztof Dąbrowski <k.fajny@gmail.com>
2018-12-28 18:37:22 +0100

    NoMovePossible exception

--- RandomPlayer/Player.h
+++ RandomPlayer/Player.h
@@ -1,9 +1,12 @@
 ﻿#pragma once
 
 #include <string>
+#include <exception>
 
 #include "Board.h"
 #include "Parser.h"
+
+typedef std::pair<std::pair<int, int>, std::pair<int, int>> Move;
 
 class Player
 {
@@ -12,5 +15,13 @@ public:
 	Player(const Board &board);
 
 	void registerMove(const std::string& move);
+	void registerMove(const Move &move);
+	Move generateMove();
 	Board& getBoard() { return board; }
+};
+
+class NoMovePossible : std::runtime_error
+{
+public:
+	NoMovePossible(const std::string& what_arg) : runtime_error(what_arg) {}
 };

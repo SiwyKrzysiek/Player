b05d7a38e52be34987a8fa5a8913429c22dfbcd6
Krzysztof Dąbrowski <k.fajny@gmail.com>
2018-12-28 18:12:39 +0100

    Register enemy move

---
+++ RandomPlayer/Player.cpp
@@ -0,0 +1,14 @@
+﻿#include "Player.h"
+
+Player::Player(const Board &board) : board(board)
+{
+
+}
+
+void Player::registerMove(const std::string& move)
+{
+	const auto moveDetails = Parser::parseMove(move);
+
+	board.takeSpot(moveDetails.first);
+	board.takeSpot(moveDetails.second);
+}
---
+++ RandomPlayer/Player.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+
+#include <string>
+
+#include "Board.h"
+#include "Parser.h"
+
+class Player
+{
+	Board board;
+public:
+	Player(const Board &board);
+
+	void registerMove(const std::string& move);
+	Board& getBoard() { return board; }
+};

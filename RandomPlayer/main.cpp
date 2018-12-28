c92706b6356b687a9f256bb02d76d7689094f4ac
Krzysztof Dąbrowski <k.fajny@gmail.com>
2018-12-28 19:30:31 +0100

    Comunication with judge

--- RandomPlayer/main.cpp
+++ RandomPlayer/main.cpp
@@ -10,19 +10,24 @@ using namespace std;
 
 int main()
 {
-	Board board(5);
-	//vector<pair<int, int>> taken = { {0,0}, {1,2}, {3,3} };
-	//board.takeManySpots(taken);
+	int boardSize;
+	cin >> boardSize;
+	cout << "ok" << endl;
 
-	const string obstacleList("{1;2},{3;3},{3;4},{5;5}");
-	board.takeManySpots(Parser::parseObstacleList(obstacleList));
+	string obstacles;
+	cin >> obstacles;
 
-	//cout << board.toString();
-
+	Board board(boardSize);
+	board.takeManySpots(Parser::parseObstacleList(obstacles));
 	Player player(board);
-	player.registerMove("{1;1},{2;1}");
 
-	cout << player.getBoard().toString() << endl;
+	cout << "ok" << endl;
+
+	string firstMove;
+	cin >> firstMove;
+
+	if (firstMove != "start")
+		player.registerMove(firstMove);
 
 	try
 	{
@@ -30,14 +35,47 @@ int main()
 		{
 			cout << Parser::moveToString(player.generateMove()) << endl;
 
+			string enemyMove;
+			cin >> enemyMove;
+			player.registerMove(enemyMove);
+
 			cout << player.getBoard().toString() << endl;
-			getchar();
 		}
 	}
 	catch (NoMovePossible e)
 	{
-		cout << "No more moves possible" << endl;
+		//cout << "No more moves possible" << endl;
+		return 0;
 	}
+
+	//Board board(5);
+	////vector<pair<int, int>> taken = { {0,0}, {1,2}, {3,3} };
+	////board.takeManySpots(taken);
+
+	//const string obstacleList("{1;2},{3;3},{3;4},{5;5}");
+	//board.takeManySpots(Parser::parseObstacleList(obstacleList));
+
+	////cout << board.toString();
+
+	//Player player(board);
+	//player.registerMove("{1;1},{2;1}");
+
+	//cout << player.getBoard().toString() << endl;
+
+	//try
+	//{
+	//	while (true)
+	//	{
+	//		cout << Parser::moveToString(player.generateMove()) << endl;
+
+	//		cout << player.getBoard().toString() << endl;
+	//		getchar();
+	//	}
+	//}
+	//catch (NoMovePossible e)
+	//{
+	//	cout << "No more moves possible" << endl;
+	//}
 	
 
 	

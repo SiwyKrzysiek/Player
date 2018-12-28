3f41e9d0a9f9a956bb7e986d76a039f81904437f
Krzysztof Dąbrowski <k.fajny@gmail.com>
2018-12-28 19:02:48 +0100

    Demo of move generation

--- RandomPlayer/main.cpp
+++ RandomPlayer/main.cpp
@@ -1,14 +1,47 @@
 #include <iostream>
+#include <vector>
+#include <utility>
 
 #include "Board.h"
+#include "Parser.h"
+#include "Player.h"
 
 using namespace std;
 
 int main()
 {
 	Board board(5);
+	//vector<pair<int, int>> taken = { {0,0}, {1,2}, {3,3} };
+	//board.takeManySpots(taken);
 
-	cout << board.toString();
+	const string obstacleList("{1;2},{3;3},{3;4},{5;5}");
+	board.takeManySpots(Parser::parseObstacleList(obstacleList));
+
+	//cout << board.toString();
+
+	Player player(board);
+	player.registerMove("{1;1},{2;1}");
+
+	cout << player.getBoard().toString() << endl;
+
+	try
+	{
+		while (true)
+		{
+			cout << Parser::moveToString(player.generateMove()) << endl;
+
+			cout << player.getBoard().toString() << endl;
+			getchar();
+		}
+	}
+	catch (NoMovePossible e)
+	{
+		cout << "No more moves possible" << endl;
+	}
+	
+
+	
+
 
 	return 0;
 }
